031be3b658b1e10c9462234d7076b9ee0c87afb2
Mohamed Zitouni <mohamed.zitouni@external.grtgaz.com>
2025-06-04 17:44:20 +0200

    implement unused methods into zparamsservice

--- lib/exportable/services/z_params_service.dart
+++ lib/exportable/services/z_params_service.dart
@@ -3,4 +3,17 @@ import 'package:z_charts/exportable/services/z_data_service.dart';
 
 abstract class ZParamsService extends ZDataService<ZParams> {
   Future<ZParams?> getByPageId(dynamic pageId);
+
+  @override
+  List<Map<String, dynamic>> adaptData(List<ZParams> data) {
+    return [];
+  }
+  @override
+  Future<ZParams> addEntity(ZParams entity) {
+    return Future.value(ZParams.empty());
+  }
+  @override
+  Future<List<ZParams>> fetchEntitiesBetween(DateTime startDate, DateTime endDate) {
+    return Future.value([]);
+  }
 }
